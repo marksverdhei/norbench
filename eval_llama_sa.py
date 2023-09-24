4c9c2819899f8b66c752bbb5175906c253c34df2
marksverdhei <>
2023-09-24 20:06:03 +0200

    Add space check for tokenizer

--- eval_llama_sa.py
+++ eval_llama_sa.py
@@ -15,7 +15,8 @@ class Config:
     # TODO: validate prompt template
     model_name: str
     prompt_template: str
-    class_names: dict
+    class_names: list
+    from_pretrained_kwargs: dict = None
     tokenizer: Optional[str] = None
 
     @classmethod
@@ -26,22 +27,37 @@ class Config:
         return cls(**config_dict)
     
 
-def get_class_token_ids(tokenizer, categories):
+def get_class_token_ids(tokenizer, categories, space_is_token=False):
     # TODO: handle cases where two categories share first tokens
-    tokens = [tokenizer.encode(f" {category}", add_special_tokens=False)[0] for category in categories]
+    idx = int(space_is_token)
+    tokens = [tokenizer.encode(f" {category}", add_special_tokens=False)[idx] for category in categories]
     return tokens
 
 
 
 def eval_sent_sa(config: Config):
-    model = AutoModelForCausalLM.from_pretrained(config.model_name, device_map="auto", trust_remote_code=True)
+    from_pretrained_kwargs = {
+        "pretrained_model_name_or_path": config.model_name,
+        "device_map": "auto", 
+        "trust_remote_code": True,
+    }
+
+    from_pretrained_kwargs.update(config.from_pretrained_kwargs or {})
+
+    model = AutoModelForCausalLM.from_pretrained(**from_pretrained_kwargs)
     model.eval()
     tokenizer = AutoTokenizer.from_pretrained(config.model_name)
     softmax = Softmax(dim=0)
     print(config.prompt_template)
     df = pd.read_csv('sentiment_analysis/sentence/test.csv.gz', compression='gzip')
     print(df)
-    class_token_ids = get_class_token_ids(tokenizer, config.class_names)
+    if len(tokenizer.encode(" a", add_special_tokens=False)) > 1:
+        space_is_token = True
+    else:
+        space_is_token = False
+
+    print(space_is_token)
+    class_token_ids = get_class_token_ids(tokenizer, config.class_names, space_is_token=space_is_token)
 
     @torch.no_grad()
     def _predict(row):
